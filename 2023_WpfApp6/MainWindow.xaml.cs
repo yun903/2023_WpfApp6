cc915a7369ac8891acc7803e5780bd19b43abd39
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-12-11 11:00:19 +0800

    非同步擷取網路資料

--- 2023_WpfApp6/MainWindow.xaml.cs
+++ 2023_WpfApp6/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,14 +18,50 @@ namespace _2023_WpfApp6
     /// </summary>
     public partial class MainWindow : Window
     {
+        string defaultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate desc&format=JSON";
+        AQIdata aqiData = new AQIdata();
+        List<Field> fields = new List<Field>();
+        List<Record> records = new List<Record>();
         public MainWindow()
         {
             InitializeComponent();
+            UrlTextBox.Text = defaultURL;
         }
 
-        private void fetchButton_Click(object sender, RoutedEventArgs e)
+        private async void fetchButton_Click(object sender, RoutedEventArgs e)
         {
+            string url = UrlTextBox.Text;
+            ContentTextBox.Text = "抓取資料...";
 
+            string data = await FetchContentAsync(url);
+            ContentTextBox.Text = data;
+            aqiData = JsonSerializer.Deserialize<AQIdata>(data);
+            fields = aqiData.fields.ToList();
+            records = aqiData.records.ToList();
+            statusTextBlock.Text = $"共有 {records.Count} 筆資料，每筆資料有{fields.Count}個欄位。";
+        }
+
+        private async Task<string> FetchContentAsync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                client.Timeout = TimeSpan.FromSeconds(200); // 設置超時時間為 200 秒
+                try
+                {
+                    return await client.GetStringAsync(url);
+                }
+                catch (TaskCanceledException ex)
+                {
+                    MessageBox.Show("請求超時或被取消");
+                    // 可以在這裡添加重試邏輯或其他錯誤處理
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"讀取數據時發生錯誤: {ex.Message}");
+                    throw;
+                }
+            }
         }
     }
 }
