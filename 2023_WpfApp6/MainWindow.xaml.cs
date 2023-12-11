eab9ca041a8368fbfe476a405506618bd7874702
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-12-11 13:39:38 +0800

    UI初步設計

--- 2023_WpfApp6/MainWindow.xaml.cs
+++ 2023_WpfApp6/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace _2023_WpfApp6
 {
@@ -19,6 +10,11 @@ namespace _2023_WpfApp6
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GetWebDataButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
