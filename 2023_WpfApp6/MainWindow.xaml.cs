3c895a2b95d940e083c571c29267c909a30b35d9
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-12-12 15:19:00 +0800

    讀取網路資料，填入DataGrid，動態產生CheckBox

--- 2023_WpfApp6/MainWindow.xaml.cs
+++ 2023_WpfApp6/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _2023_WpfApp6
 {
@@ -16,9 +10,81 @@ namespace _2023_WpfApp6
     /// </summary>
     public partial class MainWindow : Window
     {
+        string defaultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate desc&format=JSON";
+        AQIdata aqidata = new AQIdata();
+        List<Field> fields = new List<Field>();
+        List<Record> records = new List<Record>();
+
         public MainWindow()
         {
             InitializeComponent();
+            UrlTextBox.Text = defaultURL;
+        }
+
+        private async void FetchDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTextBox.Text = "抓取網路資料中...";
+
+            string jsonData = await FetchContentAsync(defaultURL);
+            ContentTextBox.Text = jsonData;
+            aqidata = JsonSerializer.Deserialize<AQIdata>(jsonData);
+            fields = aqidata.fields.ToList();
+            records = aqidata.records.ToList();
+            StatusTextBlock.Text = $"共有{records.Count} 筆資料";
+            DisplayAQIData();
+        }
+
+        private void DisplayAQIData()
+        {
+            RecordDataGrid.ItemsSource = records;
+
+            Record record = records[0];
+            DataWrapPanel.Children.Clear();
+
+            foreach (Field field in fields)
+            {
+                var propertyInfo = record.GetType().GetProperty(field.id);
+                if (propertyInfo != null)
+                {
+                    var value = propertyInfo.GetValue(record) as String;
+                    if (double.TryParse(value, out double v))
+                    {
+                        CheckBox cb = new CheckBox
+                        {
+                            Content = field.info.label,
+                            Tag = field.id,
+                            Margin = new Thickness(3),
+                            FontSize = 14,
+                            FontWeight = FontWeights.Bold,
+                            Width = 120
+                        };
+                        cb.Checked += CheckBox_Checked;
+                        DataWrapPanel.Children.Add(cb);
+                    }
+                }
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var cb = sender as CheckBox;
+
+            MessageBox.Show($"{cb.Tag}");
+        }
+
+        private async Task<string> FetchContentAsync(string url)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    return await client.GetStringAsync(url);
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"{ex.Message}";
+            }
         }
     }
 }
